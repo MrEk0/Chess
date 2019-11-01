098e915978f924462037b5a8f8641174e0ca1559
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-11-01 14:10:08 +0300

    bishop

---
+++ Chess Game/Assets/Scripts/Pieces/Bishop.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ChessGame
+{
+    public class Bishop : ChessPiece
+    {
+        Vector2Int[] moveVectors = new Vector2Int[4]
+       {
+            new Vector2Int(1,1),
+            new Vector2Int(-1,-1),
+            new Vector2Int(1,-1),
+            new Vector2Int(-1,1),
+       };
+
+        private bool isPreviousTileTaken = false;
+
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            TileManager.instance.SelectPiece(gameObject);
+            ShowPossibleSteps();
+        }
+
+        public override void ShowPossibleSteps()
+        {
+            //List<Vector2Int> moveVectorsList = new List<Vector2Int>();
+
+            for (int i = 0; i < moveVectors.Length; i++)
+            {
+                for (int j = 1; j < 9; j++)
+                {
+                    GameObject tile = TileManager.instance.GetStepTile(transform.position, moveVectors[i]*j);
+                    CheckAttackSteps(tile);
+                }
+                isPreviousTileTaken = false;
+            }
+        }
+
+        public override void CheckAttackSteps(GameObject tile)
+        {
+            if (!TileManager.instance.IsTileExist(tile))
+            {
+                return;
+            }
+
+            if (!isPreviousTileTaken)
+            {
+                if (TileManager.instance.IsTileTaken(tile))
+                {
+                    isPreviousTileTaken = true;
+
+                    if (TileManager.instance.IsPieceTheSameSide(tile, colorType))
+                    {
+                        return;
+                    }
+
+                    TileManager.instance.ChangeTileColor(tile, Color.red);
+                    TileManager.instance.AddTileToMove(tile);
+                }
+                else
+                {
+                    TileManager.instance.ChangeTileColor(tile, Color.cyan);
+                    TileManager.instance.AddTileToMove(tile);
+                }
+            }
+        }
+    }
+}
