2e57e2c618b79cbc2a19879b4441b8b22f1bfcc7
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-11-01 14:10:43 +0300

    king rook, queen, knight

---
+++ Chess Game/Assets/Scripts/Pieces/King.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace ChessGame
+{
+    public class King : ChessPiece
+    {
+        Vector2Int[] moveVectors = new Vector2Int[8]
+       {
+            new Vector2Int(1,1),
+            new Vector2Int(-1,1),
+            new Vector2Int(1,-1),
+            new Vector2Int(-1,-1),
+            new Vector2Int(0,1),
+            new Vector2Int(0,-1),
+            new Vector2Int(-1,0),
+            new Vector2Int(1,0),
+       };
+
+        public override void OnPointerClick(PointerEventData eventData)
+        {
+            TileManager.instance.SelectPiece(gameObject);
+            ShowPossibleSteps();
+        }
+
+        public override void ShowPossibleSteps()
+        {
+            for (int i = 0; i < moveVectors.Length; i++)
+            {
+                GameObject tile = TileManager.instance.GetStepTile(transform.position, moveVectors[i]);
+                CheckAttackSteps(tile);
+            }
+        }
+
+        public override void CheckAttackSteps(GameObject tile)
+        {
+            if (!TileManager.instance.IsTileExist(tile))
+            {
+                return;
+            }
+
+            if (TileManager.instance.IsTileTaken(tile))
+            {
+                if (TileManager.instance.IsPieceTheSameSide(tile, colorType))
+                {
+                    return;
+                }
+
+                TileManager.instance.ChangeTileColor(tile, Color.red);
+                TileManager.instance.AddTileToMove(tile);
+            }
+            else
+            {
+                TileManager.instance.ChangeTileColor(tile, Color.cyan);
+                TileManager.instance.AddTileToMove(tile);
+            }
+        }
+    }
+}
